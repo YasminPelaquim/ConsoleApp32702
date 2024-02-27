e813b04cd1e3c1f1053f8c50fee69c2431446d86
Usuario <Usuario@CPD02-M06>
2024-02-27 11:21:03 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp32702/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32702
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o valor do litro de combustível? : ");
+            double litro = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a distância a ser percorrida em km? : ");
+            double km = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantos km o veículo faz por litro? : ");
+            double consumo = double.Parse(Console.ReadLine());
+
+            double distancia  = km / consumo;
+
+            double gasto = distancia * litro;
+
+            Console.WriteLine("O total gasto na viagem é " + gasto);
+
+            Console.ReadKey();
+        }
+    }
+}
